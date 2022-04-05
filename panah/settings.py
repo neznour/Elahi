694d805f859ec77887a809c12f81bddb528bb574
neznour <nazinourollahi@gmail.com>
2022-04-05 11:59:02 -0400

    modified staticfiledir

--- panah/settings.py
+++ panah/settings.py
@@ -123,7 +123,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, '/tmp/build_1d0d46fe/staticfiles'),]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
