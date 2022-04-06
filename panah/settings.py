40a8a3fe15a96cd7b1dcbd05b1588108956eaaac
neznour <nazinourollahi@gmail.com>
2022-04-06 12:17:25 -0400

    modified setting

--- panah/settings.py
+++ panah/settings.py
@@ -4,12 +4,25 @@ from pathlib import Path
 import os 
 import django_heroku
 import dj_database_url
-from decouple import config
+from decouple import RepositoryIni, Config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+config = Config(RepositoryIni('path/to/settings.ini'))
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    print("Postgres URL not found, using sqlite instead")
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+DEBUG = config('DEBUG')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
