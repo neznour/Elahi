efc1dd9e48cde8985003dca579518f255f41507f
neznour <nazinourollahi@gmail.com>
2022-04-12 14:04:58 -0400

    load static

--- panah/settings.py
+++ panah/settings.py
@@ -131,8 +131,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles'),]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
+
